dcd7c946044bbeda9a682028ff0493b240edb72b
seito-developer <seito@bug-fix.org>
2021-10-26 11:49:48 +0900

    overwrite

--- ns350/2021-10-25/test.R
+++ ns350/2021-10-25/test.R
@@ -1,27 +1,24 @@
-library(lubridate)
+#LAND ICE MASS DATA ,,
+#Data from JPL RL05p1M (SS48z-CRI),,
+#Greenland GIA correction: A et al.,,
+#Antarctica GIA correction: Ivins et al.,,
+#Values are anomalies relative to timeseries mean,,
+#"Provided by David Wiese, JPL",,http://climate.nasa.gov/vital-signs/land-ice/
+#  ,,
+#Greenland Trend (2002 - 2014): -287 Gt/yr,,
+#Antarctica Trend (2002 - 2014): -134 Gt/yr,,
+#,,
 
-#Read data
-csv_path <- "../s1-student-version--ea-assignment--climate-change--land-ice-mass-data---sheet1.csv"
-data <- read.csv(csv_path, header = TRUE)
+library(ggplot2)
+file_path = "./assets/s1-student-version--ea-assignment--climate-change--land-ice-mass-data---sheet1.csv"
+data <- read.csv(file_path)
 
-#Define data
-data_time <- data[11:150, 1]
-data_gm <- data[10:150, 2]
-data_am <- data[10:150, 3]
+data$time = data[, 1] #time column can be accessed with "time"
+data$greenland = data[, 2] #greenland column
+data$antarctica = data[, 3] #antarctica column
 
-#Plot graph
-plot(data_time, data_gm, type="l", xaxt="s", yaxt="s", xlab="Time",ylab="Tempature", main="LAND ICE MASS DATA")
-par(xaxt="s")
-axis.Date(1,at=seq(min(data[,1]),max(data[,1]),"Time"),format="%y.%d")
+plot_base <- ggplot(data, aes(x=time, y=antarctica)) + labs(x = "Year", y= "Antarctica ice mass anomaly", caption = "Figure 1.  Annual land ice mass anomalies in Antarctica from 2002 until 2014")
 
-#Add color & another line
-points(data_time, data_gm, type="l", lwd=1, col="red")
-points(data_time, data_am, type="l", lwd=1, col="blue")
-
-#Put labels
-legend("bottomleft",c("Greenland mass","Antarctica mass"),
-       lwd=c(1,1),
-       col=c("red","blue"),
-       lty=c("solid","solid")
-      )
+# draw
+plot_base + geom_line(aes(x=time, y=antarctica), color="blue") + geom_line(aes(x=time, y=greenland), color="green")
 
