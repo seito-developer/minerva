8480105132144570c6e800a801b7035ebfc15377
seito <seito.horiguchi@gmail.com>
2021-12-18 20:27:55 +0900

    cleaned up

--- cs350/2021-12-19-assignment/index.R
+++ cs350/2021-12-19-assignment/index.R
@@ -1,17 +1,17 @@
-# Initial setup
-library(tidyverse) # the pakcage library for ggplot2, dplyr, ... etc
+# My function
 source('/Users/seito/Documents/develop/minerva/cs350/2021-12-19-assignment/function.R')
-#library("stringr") #
-#file_path = "./data/responses.csv"
+# The pakcage library for ggplot2, dplyr, ... etc
+library(tidyverse)
+
+# Import data
 file_path = "/Users/seito/Documents/develop/minerva/cs350/2021-12-19-assignment/assets/degrees-that-pay-back.csv"
 df1 <- read.csv(file_path, header = T)
+
 dataset <- df1[,3]
 sararies <- c()
 len <- length(dataset)
 for (i in 1:len) {
-  #sararies <- as.integer(str_sub(val, start = 2, end = -1)) * 10
   sararies <- c(sararies, dollerToInt(dataset[i]))
-  #print(dataset[i])
 }
 
 print(sararies)
