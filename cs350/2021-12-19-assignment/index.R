2dd1567fd0db5f34e010ec01bb4c0f89e8405359
seito <seito.horiguchi@gmail.com>
2021-12-19 03:42:57 +0900

    clean

--- cs350/2021-12-19-assignment/index.R
+++ cs350/2021-12-19-assignment/index.R
@@ -5,12 +5,12 @@ library(tidyverse)
 
 # Import data
 file_path = "/Users/seito/Documents/develop/minerva/cs350/2021-12-19-assignment/assets/degrees-that-pay-back.csv"
-df1 <- read.csv(file_path, header = T)
+df <- read.csv(file_path, header = T)
 
 # Set data lists
-um <- df1[,1]
-sms <- dollerToInt(df1[,2], sms)
-mcms <- dollerToInt(df1[,3], sms)
+um <- df[,1]
+sms <- dollerToInt(df[,2], sms)
+mcms <- dollerToInt(df[,3], sms)
 
 #Calculate total income for 30 years
 total_income <- (sms * 10 + mcms * 20)
@@ -25,3 +25,12 @@ for(i in 1:nsims){
 
 #Draw the chart
 hist(samplings, col = "lightblue", main = "Income for 30 years", xlab = "$")
+
+values.mean <- mean(total_income)
+values.range <- range(total_income)
+values.sd <- sd(samplings)
+cat("mean:", values.mean)
+cat("sd", values.sd)
+cat("range", values.range)
+
+
