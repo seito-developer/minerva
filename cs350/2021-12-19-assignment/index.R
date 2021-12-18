8c0655ab0a2d5d9ed5f9b14bdc79404db923fcef
seito <seito.horiguchi@gmail.com>
2021-12-19 00:21:04 +0900

    Limit Theorem worked

--- cs350/2021-12-19-assignment/index.R
+++ cs350/2021-12-19-assignment/index.R
@@ -7,26 +7,21 @@ library(tidyverse)
 file_path = "/Users/seito/Documents/develop/minerva/cs350/2021-12-19-assignment/assets/degrees-that-pay-back.csv"
 df1 <- read.csv(file_path, header = T)
 
-# Starting Median Salary * 10 years
+# Set data lists
 um <- df1[,1]
 sms <- dollerToInt(df1[,2], sms)
 mcms <- dollerToInt(df1[,3], sms)
 
-#Lifetime income
+#Calculate total income for 30 years
 total_income <- (sms * 10 + mcms * 20)
 
-#table <- data.frame(um, li)
-#print(table)
-hist(total_income, col = "lightblue", main = "Income for 30 years", xlab = "$")
-
+#Integrate samplings from the population with Central Limit Theorem
+nsims = 10000
+sample_size <- 10
 samplings <- c()
-for(i in 1:100){
-  samplings <- c(samplings, mean(sample(total_income, 10)))
+for(i in 1:nsims){
+  samplings <- c(samplings, mean(sample(total_income, sample_size, replace=TRUE)))
 }
-print(samplings)
 
-#sd(li)
-#mean(li)
-#print()
-
-#dnorm( x, mean=m, sd=n )
+#Draw the chart
+hist(samplings, col = "lightblue", main = "Income for 30 years", xlab = "$")
