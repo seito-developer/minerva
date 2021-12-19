f8418018cc8393f2a79e44af35960b85eb7fab19
seito <seito.horiguchi@gmail.com>
2021-12-19 18:23:34 +0900

    cleaned

--- cs350/2021-12-19-assignment/index.R
+++ cs350/2021-12-19-assignment/index.R
@@ -1,7 +1,18 @@
-# My function
-source('/Users/seito/Documents/develop/minerva/cs350/2021-12-19-assignment/function.R')
 # The pakcage library for ggplot2, dplyr, ... etc
 library(tidyverse)
+
+# My function
+dollerToInt <- function(dataset, data_list){
+  data_list <- c()
+  len <- length(dataset)
+  for (i in 1:len) {
+    complete.cases(dataset[i])
+    target <- str_sub(dataset[i], start = 2, end = -1)
+    num <- str_replace(target, ',', '')
+    data_list <- c(data_list, as.integer(num))
+  }
+  return(data_list)
+}
 
 # Import data
 file_path = "/Users/seito/Documents/develop/minerva/cs350/2021-12-19-assignment/assets/salaries-by-college-type.csv"
