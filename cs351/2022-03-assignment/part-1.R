c862b6d97e087b61c37fb8617326790d4dc099cd
seito <seito.horiguchi@gmail.com>
2022-03-21 01:15:33 +0900

    matrix done

--- cs351/2022-03-assignment/part-1.R
+++ cs351/2022-03-assignment/part-1.R
@@ -1,9 +1,11 @@
-library(data.table)
 install.packages("ISLR")
+install.packages("InformationValue")
+
+library(data.table)
 library(ISLR)
 library(ggplot2)
-install.packages("InformationValue")
 library(InformationValue)
+
 data(Default) #10000 observations
 set.seed(123)
 # sample 80% of the observations for the training set
@@ -12,41 +14,29 @@ training.set <- Default[training.set.index,]
 # the remaining 20% are for the test set
 test.set <- Default[-training.set.index,]
 # confirm that your results match mine
-mean(training.set$income) # 33514.55
-mean(test.set$income)     # 33526.7
+#mean(training.set$income) # 33514.55
+#mean(test.set$income)     # 33526.7
 
-# Convert Y/N to 1/0
-binary_converter <- function(data){
-  prob <- c()
-  for (item in data) {
-    if(item == 'Yes'){
-      prob <- c(prob, 1)
-    } else {
-      prob <- c(prob, 0)
-    }
-  }
-  return(prob)
-}
-
-training_def <- binary_converter(training.set$default)
-new_training_data = data.table(default = training_def, income = training.set$income, balance = training.set$balance)
-new_training_data
-my_model <- glm(formula = training_def ~ training.set$income + training.set$balance, data = new_training_data, family="binomial")
+#training
+## convert yes/no to 1/0
+training.set$default <- ifelse(training.set$default=="Yes", 1, 0)
+my_model <- glm(formula = training.set$default ~ training.set$income + training.set$balance, data = training.set, family="binomial")
 summary(my_model)
 
-plot_view <- ggplot(training.set,aes(x=training.set$balance, y=training_def)) +
-  geom_point() + 
-  geom_smooth(method = "glm", method.args= list(family="binomial"))
-plot_view
+#plot_view <- ggplot(training.set,aes(x=training.set$balance, y=training_def)) +
+#  geom_point() + 
+#  geom_smooth(method = "glm", method.args= list(family="binomial"))
+#plot_view
 
 
-#---
-test_def <- binary_converter(test.set$default)
-new_test_data = data.table(default = test_def, income = test.set$income, balance = test.set$balance)
-new_test_data
-prediction <- predict(my_model, newData = new_test_data, type="response")
+#test
+## convert yes/no to 1/0
 
-optimal <- optimalCutoff(new_test_data$default, prediction)[1]
+prediction <- predict(my_model, newData = test.set, type="response")
+test.set$default <- ifelse(test.set$default=="Yes", 1, 0)
+optimal <- optimalCutoff(test.set$default, prediction)[1]
 
-confusionMatrix(new_test_data$default, prediction)
+test_len <- length(test.set$default)
+
+confusionMatrix(test.set$default, head(prediction, n=test_len))
 
