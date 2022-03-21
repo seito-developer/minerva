1a6f903e5c6a129cbb38a68c26732f6cbbce151a
seito <seito.horiguchi@gmail.com>
2022-03-21 13:49:10 +0900

    arranged text

--- cs351/2022-03-assignment/part-1.R
+++ cs351/2022-03-assignment/part-1.R
@@ -1,3 +1,4 @@
+###### Preparation START ######
 install.packages("ISLR")
 install.packages("InformationValue")
 
@@ -8,35 +9,27 @@ library(InformationValue)
 
 data(Default) #10000 observations
 set.seed(123)
-# sample 80% of the observations for the training set
-training.set.index <- sample(1:10000, 8000, replace = FALSE)
-training.set <- Default[training.set.index,]
-# the remaining 20% are for the test set
-test.set <- Default[-training.set.index,]
-# confirm that your results match mine
-#mean(training.set$income) # 33514.55
-#mean(test.set$income)     # 33526.7
 
-#training
-## convert yes/no to 1/0
+training.set.index <- sample(1:10000, 8000, replace = FALSE) # sample 80% of the observations for the training set
+training.set <- Default[training.set.index,] # the remaining 20% are for the test set
+test.set <- Default[-training.set.index,] # confirm that your results match mine
+mean(training.set$income) # 33514.55
+mean(test.set$income)     # 33526.7
+###### Preparation END ######
+
+###### (1) START ######
+#Arranging the training data
 training.set$default <- ifelse(training.set$default=="Yes", 1, 0)
-my_model <- glm(formula = training.set$default ~ training.set$income + training.set$balance, data = training.set, family="binomial")
+my_model <- glm(formula = training.set$default ~ training.set$income + training.set$balance, 
+                data = training.set, family="binomial")
 summary(my_model)
 
-#plot_view <- ggplot(training.set,aes(x=training.set$balance, y=training_def)) +
-#  geom_point() + 
-#  geom_smooth(method = "glm", method.args= list(family="binomial"))
-#plot_view
-
-
-#test
-## convert yes/no to 1/0
-
+#Estimate the prediction and arranging the test data
 prediction <- predict(my_model, newData = test.set, type="response")
 test.set$default <- ifelse(test.set$default=="Yes", 1, 0)
 optimal <- optimalCutoff(test.set$default, prediction)[1]
-
 test_len <- length(test.set$default)
 
+#Generate the confusion matrix
 confusionMatrix(test.set$default, head(prediction, n=test_len))
-
+###### (1) END ######
