4665c93070ae220f8b3a1ed31e30136fb68d8521
seito <seito.horiguchi@gmail.com>
2022-03-21 16:57:33 +0900

    worked

--- cs351/2022-03-assignment/part-1.R
+++ cs351/2022-03-assignment/part-1.R
@@ -1,11 +1,13 @@
 ###### Preparation START ######
 install.packages("ISLR")
 install.packages("InformationValue")
+install.packages("dplyr")
 
 library(data.table)
 library(ISLR)
 library(ggplot2)
 library(InformationValue)
+library(dplyr)
 
 data(Default) #10000 observations
 set.seed(123)
@@ -13,23 +15,29 @@ set.seed(123)
 training.set.index <- sample(1:10000, 8000, replace = FALSE) # sample 80% of the observations for the training set
 training.set <- Default[training.set.index,] # the remaining 20% are for the test set
 test.set <- Default[-training.set.index,] # confirm that your results match mine
+
 mean(training.set$income) # 33514.55
 mean(test.set$income)     # 33526.7
 ###### Preparation END ######
 
 ###### (1) START ######
 #Arranging the training data
-training.set$default <- ifelse(training.set$default=="Yes", 1, 0)
-my_model <- glm(formula = training.set$default ~ training.set$income + training.set$balance, 
-                data = training.set, family="binomial")
+sampling_size = nrow(test.set)
+
+sampled_training = sample_n(tbl = training.set, size = sampling_size)
+sampled_training$default <- ifelse(sampled_training$default=="Yes", 1, 0)
+sampled_training$student <- ifelse(sampled_training$student=="Yes", 1, 0)
+my_model <- glm(formula = sampled_training$default ~ ., data = sampled_training, family="binomial")
 summary(my_model)
 
 #Estimate the prediction and arranging the test data
-prediction <- predict(my_model, newData = test.set, type="response")
-test.set$default <- ifelse(test.set$default=="Yes", 1, 0)
-optimal <- optimalCutoff(test.set$default, prediction)[1]
-test_len <- length(test.set$default)
+sampled_test = sample_n(tbl = test.set, size = sampling_size)
+sampled_test$default <- ifelse(sampled_test$default=="Yes", 1, 0)
+sampled_test$student <- ifelse(sampled_test$student=="Yes", 1, 0)
 
 #Generate the confusion matrix
-confusionMatrix(test.set$default, head(prediction, n=test_len))
+prediction <- predict(my_model, newData = sampled_test, type="response")
+optimal <- optimalCutoff(sampled_test$default, prediction)[1]
+confusionMatrix(sampled_test$default, prediction)
 ###### (1) END ######
+
