a2b7a72e0c40c6499122970ce266531e4a9111b0
seito <seito.horiguchi@gmail.com>
2022-04-13 02:40:14 +0900

    types

--- ns351/2022-04_assignment/index.R
+++ ns351/2022-04_assignment/index.R
@@ -2,8 +2,9 @@ file_path = "./dataset.csv"
 dataset <- read.csv(file_path, header = T)
 head(dataset)
 
-#Arrange dataset
+#### Arrange dataset ####
 
+# age
 new_age <- c()
 new_age <- ifelse(dataset[,2]=="15歳以下", 15, 
                           ifelse(dataset[,2]=="16 - 20歳", 18, 
@@ -22,3 +23,22 @@ new_age <- ifelse(dataset[,2]=="15歳以下", 15,
                                                                                                                      ifelse(dataset[,2]=="81歳以上", 81, FALSE 
                           )))))))))))))))
 new_age
+
+# function
+convert_eval_to_score <- function(target_col){
+  new_col <- ifelse(target_col=="強く同意する", 5,
+                ifelse(target_col=="やや同意する", 4,
+                       ifelse(target_col=="どちらとも言えない", 3,
+                              ifelse(target_col=="あまり同意しない", 2,
+                                     ifelse(target_col=="まったく同意しない", 1, FALSE
+                                     )))))
+  return(new_col)
+}
+
+## types
+new_book <- c(convert_eval_to_score(dataset[,6]))
+new_text <- c(convert_eval_to_score(dataset[,7]))
+new_video <- c(convert_eval_to_score(dataset[,8]))
+new_console <- c(convert_eval_to_score(dataset[,9]))
+new_live <- c(convert_eval_to_score(dataset[,10]))
+
