f1d9402a802de1947588303923e94c3f9060a426
seito <seito.horiguchi@gmail.com>
2022-04-20 00:48:49 +0900

    graph-2 fixed

--- ns351/2022-04_assignment/index2.R
+++ ns351/2022-04_assignment/index2.R
@@ -7,10 +7,8 @@ file_path = "./dataset.csv"
 dataset <- read.csv(file_path, header = T)
 head(dataset)
 
-#### Arrange dataset ####
-
 reasons <- c("quality", "operability", "freshness", "easy-to-understand", "easy-to-start", "activating", "asking", "others", "combination", "practicality", "dictionary")
-#reasons_col <- c(dataset$book_strength, dataset$text_strength, dataset$video_strength, dataset$console_strength, dataset$live_strength, dataset$book_weakness, dataset$text_weakness, dataset$video_weakness, dataset$console_weakness, dataset$live_weakness)
+reasons_label <- c("quality", "operability", "freshness", "e-t-u", "e-t-s", "activating", "asking", "others", "combination", "practicality", "dictionary")
 reasons_len <- length(reasons)
 dataset$book_strength
 reasons_col[1]
@@ -21,10 +19,36 @@ generate_reason_dataset <- function(col, title){
     list_values <- c(list_values, sum(col == reasons[index]))
   }
   
-  data_frame <- data.frame(reasons, list_values)
-  print(data_frame)
-  ggplot(data_frame, aes(x=reorder(reasons,-list_values) , y=list_values, fill=list_values)) + geom_bar(stat="identity") + ggtitle(title)
+  sum_votes <- sum(list_values)
+  
+  data_frame <- data.frame(reasons_label, list_values)
+  ggplot(data_frame, aes(x=reorder(reasons_label,-list_values) , y=list_values, fill=list_values)) + geom_bar(stat="identity") + ggtitle(title) + scale_y_continuous(limits = c(0, 10)) + labs(x="Reasons", y="Number o Votes", fill="Votes", caption=paste("Total Votes:",sum_votes))
 }
 
-generate_reason_dataset(dataset$book_strength, "The Reason of Book Strength")
+generate_reason_dataset(
+  dataset$book_strength, 
+  "The Reason of Book Strength"
+)
+
+generate_reason_dataset(
+  dataset$text_strength, 
+  "The Reason of Text Strength"
+)
+
+generate_reason_dataset(
+  dataset$video_strength, 
+  "The Reason of Video Strength"
+)
+
+generate_reason_dataset(
+  dataset$console_strength, 
+  "The Reason of Console Strength"
+)
+
+generate_reason_dataset(
+  dataset$live_strength, 
+  "The Reason of Live Strength"
+)
+
+
 
