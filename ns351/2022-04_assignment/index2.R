0d212d6dfbc304304891b9a780c24f1d56ebae84
seito <seito.horiguchi@gmail.com>
2022-04-20 00:24:19 +0900

    bar chart worked

--- ns351/2022-04_assignment/index2.R
+++ ns351/2022-04_assignment/index2.R
@@ -9,13 +9,13 @@ head(dataset)
 
 #### Arrange dataset ####
 
-reasons <- c("quality", "operability", "freshness", "easy-to-understand", "easy-to-start", "activating", "asking", "others", "combination", "practicality", "dictionary")
+reasons <- c("quality", "operability", "freshness", "etu", "ets", "ac", "ask", "others", "comb", "prc", "dic")
 #reasons_col <- c(dataset$book_strength, dataset$text_strength, dataset$video_strength, dataset$console_strength, dataset$live_strength, dataset$book_weakness, dataset$text_weakness, dataset$video_weakness, dataset$console_weakness, dataset$live_weakness)
 reasons_len <- length(reasons)
 dataset$book_strength
 reasons_col[1]
 
-generate_reason_dataset <- function(col){
+generate_reason_dataset <- function(col, title){
   list_values <- c()
   for (index in 1:reasons_len) {
     list_values <- c(list_values, sum(col == reasons[index]))
@@ -23,17 +23,7 @@ generate_reason_dataset <- function(col){
   
   data_frame <- data.frame(reasons, list_values)
   print(data_frame)
-  ggplot(data_frame, aes(x=reorder(reasons,list_values) , y=list_values)) + geom_bar(stat="identity")
+  ggplot(data_frame, aes(x=reorder(reasons,-list_values) , y=list_values)) + geom_bar(stat="identity") + ggtitle(title)
 }
-generate_reason_dataset(dataset$book_strength)
 
- 
-        #col=c("red", "blue", "yellow", "green", "purple"), 
- #       main="Barplot", xlab="Entry", ylab="Value", 
-        #ylim=c(0,200), 
-  #      )
-#box(lty=1)
-
-hdataset$reasons[1]
-dataset["quality"]
-sum(dataset$book_strength == "freshness")
+generate_reason_dataset(dataset$book_strength, "The Reason of Book Strength")
