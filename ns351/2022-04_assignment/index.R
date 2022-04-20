13431e0bfaae5da084022c9790559e35b4614e40
seito <seito.horiguchi@gmail.com>
2022-04-20 18:40:35 +0900

    saved

--- ns351/2022-04_assignment/index.R
+++ ns351/2022-04_assignment/index.R
@@ -13,41 +13,6 @@ head(dataset)
 eval_data <- c("strong_disagree", "disagree", "neigther", "agree", "strong_agree")
 types <- c("book", "text", "video", "console", "live")
 
-# age
-new_age <- c()
-new_age <- ifelse(dataset[,2]=="15歳以下", 15, 
-                          ifelse(dataset[,2]=="16 - 20歳", 18, 
-                                 ifelse(dataset[,2]=="21 - 25歳", 23, 
-                                        ifelse(dataset[,2]=="26 - 30歳", 28, 
-                                               ifelse(dataset[,2]=="31 - 35歳", 33, 
-                                                      ifelse(dataset[,2]=="36 - 40歳", 38, 
-                                                             ifelse(dataset[,2]=="41 - 45歳", 43, 
-                                                                    ifelse(dataset[,2]=="46 - 50歳", 48, 
-                                                                           ifelse(dataset[,2]=="51 - 55歳", 53, 
-                                                                                  ifelse(dataset[,2]=="56 - 60歳", 58, 
-                                                                                         ifelse(dataset[,2]=="61 - 65歳", 63, 
-                                                                                                ifelse(dataset[,2]=="66 - 70歳", 68, 
-                                                                                                       ifelse(dataset[,2]=="71 - 75歳", 73, 
-                                                                                                              ifelse(dataset[,2]=="76 - 80歳", 78, 
-                                                                                                                     ifelse(dataset[,2]=="81歳以上", 81, FALSE 
-                          )))))))))))))))
-# experience
-new_experience <- c()
-new_experience <- ifelse(dataset[,4]=="ない", 1,
-                         ifelse(dataset[,4]=="1年未満", 2,
-                                ifelse(dataset[,4]=="1年以上3年未満", 3,
-                                       ifelse(dataset[,4]=="3年以上5年未満", 4,
-                                              ifelse(dataset[,4]=="5年以上", 5, FALSE
-                                              )))))
-
-new_lang <- c()
-new_lang <- ifelse(dataset[,5]=="強く同意する（絶対に日本語がいい）", eval_data[5],
-                   ifelse(dataset[,5]=="やや同意する（できれば日本語がいい）", eval_data[4],
-                          ifelse(dataset[,5]=="どちらとも言えない（日本語でも英語でも気にしない）", eval_data[3],
-                                 ifelse(dataset[,5]=="あまり同意しない（できれば英語がいい）", eval_data[2],
-                                        ifelse(dataset[,5]=="まったく同意しない（絶対に英語がいい）", eval_data[1], FALSE
-                                        )))))
-
 ## types
 convert_eval_to_score <- function(target_col){
   new_col <- ifelse(target_col=="強く同意する", eval_data[5],
@@ -67,9 +32,6 @@ new_live <- c(convert_eval_to_score(dataset[,10]))
 
 # build new dataset
 new_dataset <- data.frame(
-  age = new_age,
-  experience = new_experience,
-  lang = new_lang,
   book = new_book,
   text = new_text,
   video = new_video,
@@ -85,7 +47,7 @@ scoring <- function(item){
   return(score)
 }
 
-start_col <- 4
+start_col <- 1
 type_cols_len <- ncol(new_dataset)
 scores <- c()
 
@@ -100,6 +62,16 @@ for (i_types in start_col:type_cols_len) {
 custom_types <- c(rep(types, each = length(types)))
 custom_eval  <- c(rep(eval_data, times = length(eval_data)))
 new_table <- data.frame(custom_types, custom_eval, scores)
+custom_types
+scores
+custom_eval
+
+df <- data.frame(x = custom_types, y = scores, group = custom_eval)
+ggplot(df, aes(x = x, y = y, fill = custom_eval)) + 
+  geom_bar(stat = "identity")
+
+
+
 
 ggplot(new_table, aes(x = custom_types, y = scores, fill = custom_eval, label = scores)) +
   geom_bar(stat = "identity") +
@@ -111,4 +83,8 @@ ggplot(new_table, aes(x = custom_types, y = scores, fill = custom_eval, label = 
        fill = "Evaluation")
 
 
+
+new_table
+ggplot(data = new_table, aes(x = custom_types, y = scores, fill = custom_eval)) + 
+  geom_bar(stat='identity')
   
