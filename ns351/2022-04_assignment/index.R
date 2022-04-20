7cba4f459b5215ee15406d94bd2467b251b4d0f2
seito <seito.horiguchi@gmail.com>
2022-04-20 18:44:23 +0900

    saved

--- ns351/2022-04_assignment/index.R
+++ ns351/2022-04_assignment/index.R
@@ -3,15 +3,49 @@ library("tidyr")
 library("ggplot2")
 library("stringr")
 library(RColorBrewer)
-library("plyr")
 
 file_path = "./dataset.csv"
 dataset <- read.csv(file_path, header = T)
 head(dataset)
 
-# Variables
+#### Arrange dataset ####
+
+# age
+new_age <- c()
+new_age <- ifelse(dataset[,2]=="15歳以下", 15, 
+                  ifelse(dataset[,2]=="16 - 20歳", 18, 
+                         ifelse(dataset[,2]=="21 - 25歳", 23, 
+                                ifelse(dataset[,2]=="26 - 30歳", 28, 
+                                       ifelse(dataset[,2]=="31 - 35歳", 33, 
+                                              ifelse(dataset[,2]=="36 - 40歳", 38, 
+                                                     ifelse(dataset[,2]=="41 - 45歳", 43, 
+                                                            ifelse(dataset[,2]=="46 - 50歳", 48, 
+                                                                   ifelse(dataset[,2]=="51 - 55歳", 53, 
+                                                                          ifelse(dataset[,2]=="56 - 60歳", 58, 
+                                                                                 ifelse(dataset[,2]=="61 - 65歳", 63, 
+                                                                                        ifelse(dataset[,2]=="66 - 70歳", 68, 
+                                                                                               ifelse(dataset[,2]=="71 - 75歳", 73, 
+                                                                                                      ifelse(dataset[,2]=="76 - 80歳", 78, 
+                                                                                                             ifelse(dataset[,2]=="81歳以上", 81, FALSE 
+                                                                                                             )))))))))))))))
+# experience
+new_experience <- c()
+new_experience <- ifelse(dataset[,4]=="ない", 1,
+                         ifelse(dataset[,4]=="1年未満", 2,
+                                ifelse(dataset[,4]=="1年以上3年未満", 3,
+                                       ifelse(dataset[,4]=="3年以上5年未満", 4,
+                                              ifelse(dataset[,4]=="5年以上", 5, FALSE
+                                              )))))
+# lang
 eval_data <- c("strong_disagree", "disagree", "neigther", "agree", "strong_agree")
-types <- c("book", "text", "video", "console", "live")
+
+new_lang <- c()
+new_lang <- ifelse(dataset[,5]=="強く同意する（絶対に日本語がいい）", eval_data[5],
+                   ifelse(dataset[,5]=="やや同意する（できれば日本語がいい）", eval_data[4],
+                          ifelse(dataset[,5]=="どちらとも言えない（日本語でも英語でも気にしない）", eval_data[3],
+                                 ifelse(dataset[,5]=="あまり同意しない（できれば英語がいい）", eval_data[2],
+                                        ifelse(dataset[,5]=="まったく同意しない（絶対に英語がいい）", eval_data[1], FALSE
+                                        )))))
 
 ## types
 convert_eval_to_score <- function(target_col){
@@ -32,6 +66,9 @@ new_live <- c(convert_eval_to_score(dataset[,10]))
 
 # build new dataset
 new_dataset <- data.frame(
+  age = new_age,
+  experience = new_experience,
+  lang = new_lang,
   book = new_book,
   text = new_text,
   video = new_video,
@@ -47,44 +84,47 @@ scoring <- function(item){
   return(score)
 }
 
-start_col <- 1
-type_cols_len <- ncol(new_dataset)
-scores <- c()
+book_score = scoring(new_dataset$book)
+text_score = scoring(new_dataset$text)
+video_score = scoring(new_dataset$video)
+live_score = scoring(new_dataset$live)
+console_score = scoring(new_dataset$console)
 
-for (i_types in start_col:type_cols_len) {
-  print(i_types)
-  for (i_eval in 1:length(eval_data)) {
-    scores <- c(scores, sum(new_dataset[,i_types]==eval_data[i_eval]))
+items <- c("book", "text", "video", "live", "console")
+
+new_table = data.frame(
+  strong_disagree =  c(book_score[1], text_score[1], video_score[1], live_score[1], console_score[1]),
+  disagree =  c(book_score[2], text_score[2], video_score[2], live_score[2], console_score[2]),
+  neigther =  c(book_score[3], text_score[3], video_score[3], live_score[3], console_score[3]),
+  agree =  c(book_score[4], text_score[4], video_score[4], live_score[4], console_score[4]),
+  strong_agree =  c(book_score[5], text_score[5], video_score[5], live_score[5], console_score[5])
+)
+new_table
+
+x2ratio <- function(x, byrow=FALSE) {
+  if ( !is.matrix(x) ) return(NA)  #引数xが行列以外ならば実行しない
+  if ( !is.logical(byrow) ) return(NA) #引数byrowが論理値以外ならば実行しない
+  if (byrow == FALSE)  { return(     t(  t(x) / apply(x , 2, sum)  )  )
   }
+  else return(    x / apply(x , 1, sum)  )
 }
 
-#variables: eval_data, types, scores
-custom_types <- c(rep(types, each = length(types)))
-custom_eval  <- c(rep(eval_data, times = length(eval_data)))
-new_table <- data.frame(custom_types, custom_eval, scores)
-custom_types
-scores
-custom_eval
+colnames(new_table) <- rev(eval)
+rownames(new_table) <- items
+par(xpd=TRUE) 
+plot <- barplot(x2ratio(t(new_table)), legend = FALSE, main="Histogram", xlab="Learning Types", ylab="Number of each scores", col = rev(brewer.pal(5, "Blues")))
 
-df <- data.frame(x = custom_types, y = scores, group = custom_eval)
-ggplot(df, aes(x = x, y = y, fill = custom_eval)) + 
-  geom_bar(stat = "identity")
+book_mean <- sum(new_table[1,])
+text_mean <- sum(new_table[2,])
+video_mean <- sum(new_table[3,])
+live_mean <- sum(new_table[4,])
+console_mean <- sum(new_table[5,])
 
+#text(0.7,95,paste("Score:",book_mean),pos=1)
+#text(1.9,95,paste("Score:",text_mean),pos=1)
+#text(3.1,95,paste("Score:",video_mean),pos=1)
+#text(4.3,95,paste("Score:",live_mean),pos=1)
+#text(5.5,95,paste("Score:",console_mean),pos=1)
 
+legend("topright", legend=eval, fill=brewer.pal(5, "Blues"))
 
-
-ggplot(new_table, aes(x = custom_types, y = scores, fill = custom_eval, label = scores)) +
-  geom_bar(stat = "identity") +
-  scale_fill_brewer(palette="Blues") +
-  geom_text(size = 3, position = position_stack(vjust = 0.5)) + 
-  labs(title="The Rate of Evaluation in Each Types",
-       x = "Learning Types", y = "Numbers of Votes", 
-       caption = paste("Total Votes:", sum(scores)/5),
-       fill = "Evaluation")
-
-
-
-new_table
-ggplot(data = new_table, aes(x = custom_types, y = scores, fill = custom_eval)) + 
-  geom_bar(stat='identity')
-  
